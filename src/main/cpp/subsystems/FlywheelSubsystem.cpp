7deac5b36e22605415075ab62aa42b98c868d9c9
scottcoursin <scott.coursin@gmail.com>
2022-03-11 19:39:44 -0600

    Double checked the merge

--- src/main/cpp/subsystems/FlywheelSubsystem.cpp
+++ src/main/cpp/subsystems/FlywheelSubsystem.cpp
@@ -16,7 +16,7 @@ using namespace FlywheelConstants;
 
 FlywheelSubsystem::FlywheelSubsystem() 
     : m_flywheelmotor(kPrimaryMotorPort, CANSparkMax::MotorType::kBrushless)
-    //, m_followerFlywheelMotor(kFollowerMotorPort, CANSparkMax::MotorType::kBrushless)
+    , m_followerFlywheelMotor(kFollowerMotorPort, CANSparkMax::MotorType::kBrushless)
     , m_flywheelFF(
         kS * 1_V, 
         FlywheelConstants::kV * 1_V * 1_s / 1_m, 
@@ -28,7 +28,7 @@ FlywheelSubsystem::FlywheelSubsystem()
     m_flywheelmotor.SetClosedLoopRampRate(0.0);
     m_flywheelmotor.SetInverted(true);
 
-    //m_followerFlywheelMotor.Follow(m_flywheelmotor, true);  // 2nd argument inverts the rotation
+    m_followerFlywheelMotor.Follow(m_flywheelmotor, true);  // 2nd argument inverts the rotation
 
     m_flywheelPID.SetP(kP, 0);
     m_flywheelPID.SetI(kI, 0);
@@ -130,7 +130,6 @@ void FlywheelSubsystem::CalculateRPM()
     //m_flywheelPID.SetFF(0);
 
     double FF = m_setpoint * 5.0 / 2400.0 + 0.317;
-//    double FF = m_setpoint * 4.75 / 2400.0 + 0.317;
     constexpr int pidslot = 0;
     bool bSupressFlywheel = SmartDashboard::GetBoolean("SupressFlywheel", false);
 
